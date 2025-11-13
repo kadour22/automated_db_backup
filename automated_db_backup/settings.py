fb206c57b379e43ed5c1bf74df0806946b54ec80
Abdelkader <zaroui.abedlkader777@gmail.com>
2025-11-13 17:58:21 +0100

    connect to postgres db

--- automated_db_backup/settings.py
+++ automated_db_backup/settings.py
@@ -72,13 +72,17 @@ WSGI_APPLICATION = 'automated_db_backup.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'backup_db',
+        'USER': 'postgres',
+        'PASSWORD': 'zaza',
+        'HOST': '127.0.0.1',  # or IP if remote
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
