3cf08e5d75c31a328116afb5f9434ebc04f377ab
Abdelkader <zaroui.abedlkader777@gmail.com>
2025-11-13 15:21:11 +0100

    add services app

--- automated_db_backup/settings.py
+++ automated_db_backup/settings.py
@@ -121,3 +121,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
