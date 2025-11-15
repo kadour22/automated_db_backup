b461d512e43968bed224ffc542011d5820743d59
Abdelkader <zaroui.abedlkader777@gmail.com>
2025-11-15 11:53:43 +0100

    add backup_detail service

--- services/services_job/services.py
+++ services/services_job/services.py
@@ -3,7 +3,8 @@ from services.serializers import BackupJobSerializer
 # rest framework import
 from rest_framework.response import Response
 from rest_framework import status
-
+# django import
+from django.shortcuts import get_object_or_404
 def backups_list() :
     backups = backupJob.objects.all()
     serializer = BackupJobSerializer(backups, many=True)
@@ -15,3 +16,8 @@ def create_manual_backup(request) :
         serializer.save()
         return Response(serializer.data ,status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=404)
+
+def backup_detail_view(request,backup_id) :
+    backup = get_object_or_404(backupJob,id=backup_id)
+    serializer = BackupJobSerializer(backup) 
+    return Response(serializer.data, status.HTTP_200_OK)
--- services/views.py
+++ services/views.py
@@ -2,9 +2,9 @@ from .services_job.services import *
 from rest_framework.views import APIView
 
 class backup_api_list(APIView) :
-    def get(self, request) :
+    def get(self, request,backup_id=None) :
+        if backup_id is not None:
+            return backup_detail_view(request,backup_id)
         return backups_list()
-    
     def post(self,request):
         return create_manual_backup(request)
-
