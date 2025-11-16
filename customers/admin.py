58e0f53d1d41ece762177ffbb17cd7ddc4a25375
Abdelkader <zaroui.abedlkader777@gmail.com>
2025-11-16 11:39:38 +0100

    register domain nd client models to admin panel

--- customers/admin.py
+++ customers/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Domain, Client
+from django_tenants.admin import TenantAdminMixin
 
-# Register your models here.
+@admin.register(Client)
+class ClientAdmin(TenantAdminMixin, admin.ModelAdmin) :
+    list_display = ["name"]
+
+
+@admin.register(Domain)
+class ClientAdmin(TenantAdminMixin, admin.ModelAdmin) :
+    pass
