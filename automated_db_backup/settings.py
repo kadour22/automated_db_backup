a110d6c26ba590af21a8006f272a441e45a289a3
Abdelkader <zaroui.abedlkader777@gmail.com>
2025-11-16 10:54:22 +0100

    add tenants middleware

--- automated_db_backup/settings.py
+++ automated_db_backup/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'django_tenants.middleware.main.TenantMainMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
