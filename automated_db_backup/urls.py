8df781a6285d87a3706e3a0bdb49445408ce95ba
Abdelkader <zaroui.abedlkader777@gmail.com>
2025-11-16 10:50:35 +0100

    install django tenants && update requirements.file

--- automated_db_backup/urls.py
+++ automated_db_backup/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("backups/", include('services.urls'))
+    path("backups/", include('services.urls')),
 ]
