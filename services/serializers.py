32c189acb93b478f8b48fba417a2c82e28a1b19d
Abdelkader <zaroui.abedlkader777@gmail.com>
2025-11-17 12:59:14 +0100

    add backup history model

--- services/serializers.py
+++ services/serializers.py
@@ -7,3 +7,14 @@ class BackupJobSerializer(serializers.ModelSerializer) :
         fields = [
             "db_type","db_name","db_host","db_port","db_user","db_password","schedule_type"
         ]
+
+
+        {
+            "db_type":"postgres",
+            "db_name":"backup_db",
+            "db_host":"localhost",
+            "db_port":"5432",
+            "db_user":"postgres",
+            "db_password":"zaza",
+            "schedule_type":"manual"
+        }
