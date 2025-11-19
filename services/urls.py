6d25a232946cbca44570ca135ff21808b6f702fa
Abdelkader <zaroui.abedlkader777@gmail.com>
2025-11-19 10:01:34 +0100

    add token obtain pair view

--- services/urls.py
+++ services/urls.py
@@ -1,3 +1,4 @@
+from rest_framework_simplejwt.views import TokenObtainPairView
 from django.urls import path
 from . import views
 
@@ -8,4 +9,6 @@ urlpatterns = [
     path('backups/<int:backup_id>/', views.backup_api_list.as_view(), name='backup-detail-delete'),
     # Trigger manual backup task
     path('backups/<int:job_id>/run/', views.ManualBackup.as_view(), name='backup-run'),
+    # Obtain JWT token
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair') ,
 ]
