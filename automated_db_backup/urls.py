c7f159cb65b23eb86eeb4132be8e37ca6f27cf39
Abdelkader <zaroui.abedlkader777@gmail.com>
2025-11-19 10:06:02 +0100

    configure customers app route

--- automated_db_backup/urls.py
+++ automated_db_backup/urls.py
@@ -5,4 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("backups/", include('services.urls')),
+    path("customers/", include('customers.urls')),
 ]
--- services/urls.py
+++ services/urls.py
@@ -1,4 +1,3 @@
-from rest_framework_simplejwt.views import TokenObtainPairView
 from django.urls import path
 from . import views
 
@@ -9,6 +8,4 @@ urlpatterns = [
     path('backups/<int:backup_id>/', views.backup_api_list.as_view(), name='backup-detail-delete'),
     # Trigger manual backup task
     path('backups/<int:job_id>/run/', views.ManualBackup.as_view(), name='backup-run'),
-    # Obtain JWT token
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair') ,
 ]
