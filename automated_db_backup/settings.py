b4c3ee60f173b896fe0032af1edf6f2e91cf7a38
Abdelkader <zaroui.abedlkader777@gmail.com>
2025-11-19 09:58:50 +0100

    configure jwt

--- automated_db_backup/settings.py
+++ automated_db_backup/settings.py
@@ -149,9 +149,11 @@ TENANT_MODEL = "customers.Client"
 TENANT_DOMAIN_MODEL = "customers.Domain"
 
 REST_FRAMEWORK = {
+
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
+    
 }
 
 SIMPLE_JWT = {
